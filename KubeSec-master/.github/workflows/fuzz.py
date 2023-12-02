cf08ffc2e401b1753f1fddb6b3fa27eb23a0a7e4
WilsonSinclair <wilsonsinclair6@gmail.com>
2023-12-01 22:08:42 -0600

    Added logging screenshot and some logging code to the fuzz.py file

--- KubeSec-master/.github/workflows/fuzz.py
+++ KubeSec-master/.github/workflows/fuzz.py
@@ -1,5 +1,7 @@
 # fuzz.py
 
+import logging
+logging.basicConfig(filename="fuzzlog.log", filemode='a', format='%(asctime)s %(message)s')
 from hypothesis import given
 from hypothesis.strategies import text, integers, lists
 from example_module import multiply_numbers, divide_numbers, concatenate_strings, find_maximum, square_root
@@ -18,7 +20,9 @@ def test_divide_numbers(a, b):
     try:
         divide_numbers(a, b)
     except Exception as e:
+        inputs = f"{a} and {b}"
         print(f"Bug discovered in divide_numbers with inputs ({a}, {b}): {e}")
+        logging.info("Error dividing inputs: %s", inputs)
 
 # Fuzz concatenate_strings with text inputs
 @given(text(), text())
@@ -35,6 +39,7 @@ def test_find_maximum(lst):
         find_maximum(lst)
     except Exception as e:
         print(f"Bug discovered in find_maximum with input {lst}: {e}")
+        logging.info("Error finding maximum with : %s", lst)
 
 # Fuzz square_root with positive integers
 @given(integers(min_value=0))
@@ -45,6 +50,8 @@ def test_square_root(n):
         print(f"Bug discovered in square_root with input {n}: {e}")
 
 if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
     test_multiply_numbers()
     test_divide_numbers()
     test_concatenate_strings()
